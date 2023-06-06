d861825b8274f6bec90ba248b3e32a83015731b9
Leroy Hughes <artistpiggy@gmail.com>
2023-06-06 12:13:49 +1000

    added js file

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// window.onload = function() {
+//     let c = document.getElementById("myCanvas");
+//     let ctx = c.getContext("2d");
+//     let img = document.getElementById("scream");
+//     ctx.drawImage(img, 10, 10);
+//   }; 
+
+window.onload = function() {
+    let myCanvas = document.getElementById("myCanvas");
+    let ctx d= myCanvas.getContext("2d");
+    let tractor = new Image();
+    tractor.src = "../imgs/tractor.png";
+    tractor.onload= "function"
+    ctx.draw 
+}
