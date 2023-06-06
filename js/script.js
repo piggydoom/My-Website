509b6aa19216ece0b262bace061798e0d69b3553
Matthew Robertson <autoknow.matt@gmail.com>
2023-06-06 12:28:47 +0930

    linked js file

--- js/script.js
+++ js/script.js
@@ -1,18 +1,10 @@
-// window.onload = function() {
-//     let c = document.getElementById("myCanvas");
-//     let ctx = c.getContext("2d");
-//     let img = document.getElementById("scream");
-//     ctx.drawImage(img, 10, 10);
-//   }; 
-
-window.onload = function () {
+function startGame(){
+    let tractor;
     let myCanvas = document.getElementById("myCanvas");
     let ctx = myCanvas.getContext("2d");
-
-}
-
-let tractor = new Image();
-tractor.src = "../imgs/tractor.png";
-tractor.onload = function () {
-    ctx.drawImage(tractor, ctx.width / 2, ctx.height / 2, 50, 50);
-}
+    tractor = new Image();
+    tractor.src = "../imgs/tractor.png";
+    tractor.onload = function () {
+        ctx.drawImage(tractor, 50, 50, 50, 50);
+    }
+};
